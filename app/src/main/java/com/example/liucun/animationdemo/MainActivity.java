d2a8e3cf37800ac45a11481f66465c35e75d3cd9
liucun <cun.liu@blemobi.com>
2017-11-06 09:55:50 +0800

    添加文件

--- app/src/main/java/com/example/liucun/animationdemo/MainActivity.java
+++ app/src/main/java/com/example/liucun/animationdemo/MainActivity.java
@@ -1,64 +1,50 @@
 package com.example.liucun.animationdemo;
 
-import android.graphics.drawable.AnimationDrawable;
-import android.graphics.drawable.Drawable;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.ApplicationInfo;
 import android.os.Bundle;
-import android.os.Handler;
-import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
-import android.widget.ImageView;
-import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-    private ImageView imageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        imageView = (ImageView) findViewById(R.id.img_dog);
-    }
-
-    public void methodXml(View view) {
-        imageView.setImageResource(R.drawable.dog_anim);
-        AnimationDrawable animationDrawable = (AnimationDrawable) imageView.getDrawable();
-        animationDrawable.setOneShot(false);// 设置是否只循环一次，默认为false，表示无限循环
-        animationDrawable.start();
-        frameAnimationFinishListener(animationDrawable);
-    }
-
-    public void methodJavaCode(View view) {
-        AnimationDrawable animationDrawable = new AnimationDrawable();
-        int[] anims = new int[] {R.mipmap.dog_0, R.mipmap.dog_1, R.mipmap.dog_2, R.mipmap.dog_3,
-                R.mipmap.dog_4, R.mipmap.dog_5, R.mipmap.dog_0};
-        for (int i = 0; i < anims.length; i++) {
-            Drawable drawable = ContextCompat.getDrawable(this, anims[i]);
-            animationDrawable.addFrame(drawable, 200);
-        }
-        animationDrawable.setOneShot(true);//只运行一次
-        imageView.setBackgroundDrawable(animationDrawable);
-        animationDrawable.start();
-
-
-        frameAnimationFinishListener(animationDrawable);
-    }
-
-    private void frameAnimationFinishListener(AnimationDrawable animationDrawable) {
-        // 监听帧动画运行完成
-        long duration = 0;
-        for (int i = 0; i < animationDrawable.getNumberOfFrames(); i++) {
-            duration += animationDrawable.getDuration(i);
-        }
-        final long finalDuration = duration;
-        new Handler().postDelayed(new Runnable() {
+        findViewById(R.id.bt_frame).setOnClickListener(new View.OnClickListener() {
             @Override
-            public void run() {
-                Toast.makeText(MainActivity.this, "动画运行完成" + finalDuration + "毫秒",
-                        Toast.LENGTH_SHORT).show();
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this, FrameActivity.class));
             }
-        }, duration);
+        });
+        findViewById(R.id.bt_tween).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this, TweenActivity.class));
+            }
+        });
+        findViewById(R.id.bt_property).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this, PropertyActivity.class));
+                Log.e("----------", isApkInDebug(MainActivity.this) + "");
+            }
+        });
+
+
     }
+
+    public static boolean isApkInDebug(Context context) {
+        try {
+            ApplicationInfo info = context.getApplicationInfo();
+            return (info.flags & ApplicationInfo.FLAG_DEBUGGABLE) != 0;
+        } catch (Exception e) {
+            return false;
+        }
+    }
+
 }
