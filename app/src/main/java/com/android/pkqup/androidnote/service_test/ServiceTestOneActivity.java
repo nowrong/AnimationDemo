1b0f79185f86687197f4d718c1d7391f11280f8d
liucun <cun.liu@blemobi.com>
2017-11-13 11:45:14 +0800

    添加view绘制

---
+++ app/src/main/java/com/android/pkqup/androidnote/service_test/ServiceTestOneActivity.java
@@ -0,0 +1,11 @@
+package com.android.pkqup.androidnote.service_test;
+
+import com.android.pkqup.androidnote.abase.BaseActivity;
+
+/**
+ * Created by LiuCun on 2017/11/13.<br>
+ * Describe
+ */
+
+public class ServiceTestOneActivity extends BaseActivity {
+}
