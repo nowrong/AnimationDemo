a0401b9ccf86846f3c3c07b91763c473f6417e71
liucun <cun.liu@blemobi.com>
2017-11-14 18:32:43 +0800

    添加service

---
+++ app/src/main/java/com/android/pkqup/androidnote/content_provider_test/ContentProviderActivity.java
@@ -0,0 +1,20 @@
+package com.android.pkqup.androidnote.content_provider_test;
+
+import android.os.Bundle;
+
+import com.android.pkqup.androidnote.R;
+import com.android.pkqup.androidnote.abase.BaseActivity;
+
+/**
+ * Created by LiuCun on 2017/11/14.<br>
+ * Describe
+ */
+
+public class ContentProviderActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.content_provider_activity);
+    }
+}
